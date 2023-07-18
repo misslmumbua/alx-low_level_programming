aac3ed8d23fcc15e1668213a8d5764d994479341
misslmumbua <laura.mumbua@gmail.com>
2023-07-18 05:48:44 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	while (a <= 10)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
