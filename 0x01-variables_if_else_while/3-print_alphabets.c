dd88bb4e43d6c2f49692329766f34dc0f7a1eeee
misslmumbua <laura.mumbua@gmail.com>
2023-07-18 04:55:44 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,20 @@
 /**
  * main - Entry point
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char low;
-	for(low = 'a'; low <= 'z'; low++);
+
+	for (low = 'a'; low <= 'z'; low++)
 	{
 		putchar(low);
 	}
-	for(low = 'A'; low <= 'Z'; low++);
+	for (low = 'A'; low <= 'Z'; low++)
 	{
 		putchar(low);
-		putchar('\n');
 	}
+	putchar('\n');
 	return (0);
 }
