e1ba1dbdeb7e815f3c0bc9ca4d7bf49c5a020ad4
misslmumbua <laura.mumbua@gmail.com>
2023-07-18 04:31:35 -0700

    alphABET

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-	char y;
-        for (y = 'a'; y <= 'z'; y++)
+	char *y;
+	for (y = *'a'; y <= *'z'; y++);
 	{
 		putchar(y);
+		putchar('\n');
+		
 	}
-	putchar('\n');
-        return (0);
-}     
+	return (0);
+}
