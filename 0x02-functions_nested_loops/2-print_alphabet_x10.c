0a3e4a8b0aeeb87d9dd08fab0c2307ac54132c64
misslmumbua <laura.mumbua@gmail.com>
2023-07-20 09:50:46 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char y;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
